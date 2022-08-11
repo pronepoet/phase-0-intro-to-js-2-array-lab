99020a1300f693aa1ff771da99be081588e61a1c
Pronepoet <ochiengphillip4@gmail.com>
2022-08-11 17:16:29 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph){
+cats.push('Ralph');
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(Broom){
+return [...cats, Broom]
+}
+
+function prependCat(Arnold){
+    return [Arnold, ...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
